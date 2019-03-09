26662b7c76bb6872e9ca2c25bfbe81e1bf825dc8
John Echo <john.jr.echo@gmail.com>
2019-03-09 17:53:28 -0600

    adding some styling, getting total order to hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ shopperName }) => (
   <header className="top">
     <h1>Hello {shopperName}</h1>
-    <p>Ready to order? Your recommended order are below!</p>
+    <p>Ready to order? Your recommended products are below!</p>
   </header>
 );
 export default Header;
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 class Order extends Component {
+  formatPrice = str => {
+    const [dollarSign, ...rest] = str;
+    return `${parseFloat(rest.join('')).toFixed(2)}`;
+  };
+
   render() {
     const orderIds = Object.keys(this.props.order);
     const orderTotal = orderIds.reduce((acc, currentValue) => {
       const product = this.props.products[currentValue];
       const count = this.props.order[currentValue];
-      return acc + count * product.price;
+      console.log(acc + count * this.formatPrice(product.price));
+      return acc + count * this.formatPrice(product.price);
     }, 0);
-    return <div>order</div>;
+    return <div>Your total is: ${orderTotal}</div>;
   }
 }
 
